8b84d641bbb3c1c1753e0f37416d02d308f3a7ec
Jackson Brumbaugh <jcb5582@gmail.com>
2022-06-20 00:49:40 -0400

    2022-06-20 ReVamp
    set up new directory structure

--- PerOffTool.psm1
+++ PerOffTool.psm1
@@ -1,7 +1,26 @@
-# $ExcludeList = "Test-PercentOffParamPath"
-Get-ChildItem $PSScriptRoot\*.ps1 -Exclude $ExcludeList | ForEach-Object {
-    . $_.FullName
+$ModuleHome = $PSScriptRoot
+
+$DirsToLoad = @(
+  "Public",
+  "Private"
+)
+
+$PathsToLoad = $DirsToLoad | ForEach-Object {
+  Join-Path $ModuleHome $_
 }
 
-# Default the Percentage off to 10% when 1st imported
-Set-PercentOff 0.1 | Out-Null
+$PathsToLoad | ForEach-Object {
+  $Path = $_
+  if ( -not (Test-Path $Path) ) {
+    Write-Warning ( "Failed to locate " + $Path )
+  } else {
+    Get-ChildItem $Path\*.ps1 | ForEach-Object {
+      . $_.FullName
+    }
+  }
+}
+
+Get-ChildItem $PSScriptRoot\Public | ForEach-Object {
+  $FuncName = $_.BaseName
+  Export-ModuleMember -Function $FuncName
+}
